154fd26b04596f3b17c6a79df196fb2420361cc6
preyelg <geeftedpl@gmail.com>
2025-05-28 15:23:48 -0500

    version8

--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -3,25 +3,24 @@ import json
 def lambda_handler(event, context):
     print("Event received:", json.dumps(event))
 
-    # Check if from API Gateway (HTTP POST)
     if "body" in event:
         try:
-            body_data = json.loads(event["body"])
-            print("Parsed API Gateway Body:", body_data)
+            data = json.loads(event["body"])
+            print("Parsed API body:", data)
             return {
                 "statusCode": 200,
                 "body": json.dumps({
                     "message": "API POST successful",
-                    "received": body_data
+                    "received": data
                 })
             }
         except Exception as e:
+            print("API body parse error:", str(e))
             return {
                 "statusCode": 400,
-                "body": json.dumps({ "error": "Invalid JSON", "details": str(e) })
+                "body": json.dumps({ "error": str(e) })
             }
 
-    # Check if from S3
     elif "Records" in event and event["Records"][0].get("eventSource") == "aws:s3":
         print("S3 Event Triggered:", json.dumps(event))
         return {
@@ -31,5 +30,5 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 400,
-        "body": "Unsupported event format."
+        "body": "Unrecognized event format"
     }
