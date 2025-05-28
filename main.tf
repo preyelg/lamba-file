9857cab4520b96605cfb73a520bddc83509750f4
preyelg <geeftedpl@gmail.com>
2025-05-28 15:43:47 -0500

    updated

--- main.tf
+++ main.tf
@@ -59,16 +59,24 @@ resource "aws_lambda_permission" "allow_s3" {
   source_arn    = aws_s3_bucket.upload_bucket.arn
 }
 
+resource "aws_lambda_permission" "allow_apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.file_processor.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
 resource "aws_apigatewayv2_api" "http_api" {
   name          = "lambda-http-api"
   protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id           = aws_apigatewayv2_api.http_api.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.file_processor.invoke_arn
-  integration_method = "POST"
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.file_processor.invoke_arn
+  integration_method     = "POST"
   payload_format_version = "2.0"
 }
 
